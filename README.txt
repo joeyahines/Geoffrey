c7eed7fe710332ce14f52c2c2fccf113816564d5
Joey Hines <joey.ahines@gmail.com>
2018-08-20 17:54:14 -0500

    Added setup.py and reorganized the project.

---
+++ README.txt
@@ -0,0 +1,4 @@
+Geoffrey (pronounced JOFF-ree) started his life as an inside joke none of you will understand.
+At some point, she was to become an airhorn bot. Now, they know where your stuff is.
+
+Please respect Geoffrey, the bot is very sensitive.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Geoffrey',
+    version=__import__('geoffrey').__version__,
+    packages=['geoffrey'],
+    install_requires=['discord.py', 'SQLAlchemy', 'pymysql'],
+    long_description=open('README.txt').read(),
+    entry_points={
+        'console_scripts':
+            ['geoffrey = geoffrey.Geoffrey:main',
+             ]}
+)
